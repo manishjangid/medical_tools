57156040c59a32e6fb5973261d4c649052834319
Manish Kumar <manish.jangid08@gmail.com>
2020-06-30 13:06:05 -0700

    Version 2 : Fixing bugs, support for unicode files
    Changes
    1. Support for more than 256 columns by writing the output to xlsx format.
    2. Support for python 3.8 platform

--- ExcelScraper.py
+++ ExcelScraper.py
@@ -3,21 +3,26 @@ import numpy as np
 from os.path import basename
 import os, collections, csv
 
+
 data_frame_last0 = []
 data_frame_last1 = []
 data_frame_last3 = []
 data_frame_lastminus1 = []
 numberOfSheets = 4
 
+index=0
+
 cwd = os.path.abspath('')
-print cwd
+print(cwd)
 files = os.listdir(cwd) 
-print files
+print(files)
 for filen in files:
-    print filen
+    print(filen)
+    print(index)
+    index = index + 1
     if filen.endswith('.XLS'):
         data = pd.read_excel(filen, Sheet1 = 'd-', header=0)
-        print data.head(2)
+        print(data.head(2))
         data_frame_lastminus1.append(data)
         data = pd.read_excel(filen, Sheet1 = 'd0', header=0)
         data_frame_last0.append(data)
@@ -26,12 +31,12 @@ for filen in files:
         data = pd.read_excel(filen, Sheet1 = 'd3', header=0)
         data_frame_last3.append(data)
         
-final_list = ["last-1.xls", "last0.xls","last1.xls","last3.xls"]
+final_list = ["last-1.xlsx", "last0.xlsx","last1.xlsx","last3.xlsx"]
 data_frame_lastminus1 = pd.concat(data_frame_lastminus1)
 data_frame_lastminus1.to_excel(final_list[0], index=False)
 data_frame_last0 = pd.concat(data_frame_last0)
 data_frame_last0.to_excel(final_list[1], index = False)
 data_frame_last1 = pd.concat(data_frame_last1)
-data_frame_last1.to_excel(final_list[2])
+data_frame_last1.to_excel(final_list[2], index = False)
 data_frame_last3 = pd.concat(data_frame_last3)
-data_frame_last3.to_excel(final_list[3])
+data_frame_last3.to_excel(final_list[3], index = False)
